29c99d765aca1d7d015bbc3ff5a23027f0ff6795
Castracani <willmyers993@gmail.com>
2018-07-01 15:32:36 -0400

    hope this helps

---
+++ assets/javascript/code.js
@@ -0,0 +1,18 @@
+// steam api key 597FC535B0A81C139B5227A808EAA15B
+
+$(document).ready(function() {
+
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBZvq3N02QOEG6fa-6qiDLwOHoSeIBza0Q",
+  authDomain: "game-website-9813e.firebaseapp.com",
+  databaseURL: "https://game-website-9813e.firebaseio.com",
+  projectId: "game-website-9813e",
+  storageBucket: "game-website-9813e.appspot.com",
+  messagingSenderId: "70919236665"
+};
+firebase.initializeApp(config);
+
+});
+
