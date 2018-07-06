f5b334706221220cef12f6a42d5f744aea984820
Castracani <willmyers993@gmail.com>
2018-07-06 16:21:46 -0400

    good lord

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,6 +1,6 @@
 
-
-$(document).ready(function () {
+// firebase
+  // Initialize Firebase
   var config = {
     apiKey: "AIzaSyBZvq3N02QOEG6fa-6qiDLwOHoSeIBza0Q",
     authDomain: "game-website-9813e.firebaseapp.com",
@@ -10,178 +10,143 @@ $(document).ready(function () {
     messagingSenderId: "70919236665"
   };
   firebase.initializeApp(config);
-
   var database = firebase.database();
 
-  var curUser = {
-    username: "",
-    firstName: "",
-    lastName: "",
-    email: "",
-    avatar: "",
-    steamName: "",
-    psnName: "",
-    xboxName: "",
-    nintendoId: "",
-    aboutMe: "",
-    status: "",
-    steamOnline: "",
-    steamLastOnline: ""
+  // maybe set these values initally to the firebase values?
+var curUser = {
+  username: "", 
+  firstName: "",
+  lastName: "",
+  email: "",
+  avatar: "",
+  steamName: "",
+  psnName: "",
+  xboxName: "",
+  nintendoId: "",
+  aboutMe: "",
+  status: "",
+  steamOnline: "",
+  steamLastOnline: ""
+}
+
+
+
+$(document).ready(function () {
+
+
+  
+
+  // giantbomb API start (for game library) =================================================================>>>
+  
+  var gameLibrary = [];
+  var searchResults = [];
+
+  function addGames(game) {
+
+
+    $.ajax({
+      type: 'GET',
+      dataType: 'jsonp',
+      crossDomain: true,
+      jsonp: 'json_callback',
+      url: 'https://www.giantbomb.com/api/search/?format=jsonp&api_key=47d89cf2776025d8ace3e66e641a4eb8bd066fc5&query=' + game
+    }).done(function (data) {
+      var results = data.results
+      console.log(results)
+      $("#search-results").html("")
+      // clear search array
+      searchResults = [];
+      for (i = 0; i < 10; i++) {
+        //  game icon
+        var gameImg = results[i].image.icon_url;
+        var image = $("<img src='" + gameImg + "' />")
+        //  game name
+        var gameName = $("<h3>").text(results[i].name)
+        //  is there a release date available? if so...
+        if (results[i].original_release_date !== undefined) {
+          //  format release date
+          var date = new Date(results[i].original_release_date);
+          var newDate = date.toString('dd-MM-yy')
+          // gets rid of unnecessary data
+          newDate = newDate.replace(/00:00.*/, "");
+          // save the release date
+          var gameRelease = $("<p>").text("Release date: " + newDate);
+          // if there is no release date, return 'unknown'
+        } else if (results[i].original_release_date == undefined) {
+          var gameRelease = $("<p>").text("Release date: unknown");
+        }
+        //  game description
+        var gameInfo = $("<p>").text("Description: " + results[i].deck);
+        //  add library button
+        var newButton = $("<button>" + "add to library" + "</button>");
+        newButton.addClass("add-game");
+        newButton.val([i]);
+        //  create a horizontal line
+        var hr = $("<hr>")
+        //  push game to search array
+        searchResults.push(results[i].name);
+
+        $("#search-results").append(gameName, image, gameInfo, gameRelease, newButton, hr)
+      }
+
+    }).fail(function () {
+      console.log("error");
+    }).always(function () {
+
+    });
+
   }
 
-  $('.carousel.carousel-slider').carousel({
-    fullWidth: true,
-    indicators: true,
-    next: 3
-  });
+    // game search input+++++++++++++++++++++++++++++++++++++++++
 
-  setInterval(function () {
-    $('.carousel').carousel('next');
-  }, 5000);
+  $("#search-game").on("click", function (event) {
+    event.preventDefault();
+    var inputGame = $("#search-input").val().trim();
+    //  console.log(inputGame)
+    addGames(inputGame);
+  })
 
-  $('.tabs').tabs({ swipeable: true });
 
-  $('.modal').modal();
+    // add game to library
+  
+    $(document).on("click", ".add-game", function () {
+    event.preventDefault();
+    var x = $(this).val(); //grabs value that will match location within array and assigns to a new variable
 
-  $('#profile-modal').on("click", function () {
+    var newGame = searchResults[x]; //grabs game title from searchResults array
+    gameLibrary.push(newGame); //adds to libray variable
+    console.log(gameLibrary)
+  
+    
+    // store within firebase !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    database.ref("users/" + curUser.username).update({
+      gamelibrary: gameLibrary
+    })
+  })
 
-    $('#modal1').modal('open');
-
-  });
-
-  $("#formValidate").validate({
-    rules: {
-      firstname: {
-        required: true
-      },
-      lastname: {
-        required: true
-      },
-      username: {
-        required: true,
-        minlength: 5
-      },
-      pwd: {
-        required: true,
-        minlength: 5
-      },
-      conf_pwd: {
-        required: true,
-        minlength: 5,
-        equalTo: "#id_pwd"
-      },
-    },
-    //For custom messages
-    messages: {
-      firstname: { required: "Enter a First Name" },
-      lastname: { required: "Enter a Last Name" },
-      username: {
-        required: "Enter a Last Name",
-        minlength: "Must be at least 5 characters"
-      },
-      pwd: {
-        required: "Enter a Password",
-        minlength: "Must be at least 5 characters"
-      },
-      conf_pwd: {
-        required: "Re-Enter Password",
-        minlength: "Must be at least 5 characters",
-        equalTo: "Password does not match"
-      }
-
-    },
-    errorElement: 'div',
-    errorPlacement: function (error, element) {
-      var placement = $(element).data('error');
-      if (placement) {
-        $(placement).append(error)
-      } else {
-        error.insertAfter(element);
-      }
+  $(document).on("click", "#library-refresh", function(){
+    event.preventDefault();
+    var list = $("<ul>");
+    $("#library").append(list);
+    for(i=0; i<gameLibrary.length; i++){
+      
+      var listItem = $("<li>");
+      listItem.text(gameLibrary[i]);
+      $("#library").append(listItem);
     }
-  });
+  })
 
 
-  firebase.auth().onAuthStateChanged(function (user) {
-    var user = firebase.auth().currentUser;
-    var email, uid;
-
-    // curUser.firstName = $("#firstname").val();
-    // curUser.lastName = $("#lastname").val();
-    // curUser.username = $("#username").val();
-    // curUser.email = $("#email").val();
-    // curUser.steamName = $("#steam-name").val();
-    // curUser.psnName = $("#psn-name").val();
-    // curUser.xboxName = $("#gamer-tag").val();
-    // curUser.nintendoId = $("#nintendo-id").val();
-    // curUser.aboutMe = $("#about-input").val();
+  // giantbomb API end <<<=================================================================
 
 
 
-    if (!user) {
-      event.preventDefault();
-    }
-    else if (user) {
-      // User is signed in.
-      console.log("There's a user signed in!");
-      $("#logged-in").css("display", "inline-block");
-      $("#not-logged-in").css("display", "none");
 
-      var email = user.email;
-      var isAnonymous = user.isAnonymous;
-      var uid = user.uid;
-      var providerData = user.providerData;
-      // var database = firebase.database();
-      // database.ref('users/' + uid).set({
-      //   username: curUser.username,
-      //   firstname: curUser.firstName,
-      //   lastName: curUser.lastName,
-      //   email: curUser.email,
-      //   steamName: curUser.steamName,
-      //   psnName: curUser.psnName,
-      //   xboxName: curUser.xboxName,
-      // });
-
-      console.log(email, isAnonymous, uid, providerData);
-      localStorage.setItem("username", username);
-      localStorage.setItem("UID", uid);
+  // Profile creation start ============================================================>>>
 
 
-      $("#welcome-text").text(" Happy to have you here, " + email + "!");
-      console.log(name, email, uid);
-
-    } else {
-      // No user is signed in.
-      console.log("Nobody's here!");
-      $("#logged-in").css("display", "none");
-      $("#not-logged-in").css("display", "inline-block");
-      $("#welcome-text").empty();
-    }
-  });
-
-  $("#submit-btn").click(function (user) {
-    console.log("I'm working!!!");
-    var email = $("#email").val().trim();
-    var password = $("#id_pwd").val().trim();
-    var
-    if (user != null) {
-      firebase.auth().signOut();
-    }
-    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
-      .then(function () {
-        // Existing and future Auth states are now persisted in the current
-        // session only. Closing the window would clear any existing state even
-        // if a user forgets to sign out.
-        // ...
-        // New sign-in will be persisted with session persistence.
-        return firebase.auth().createUserWithEmailAndPassword(email, password);
-      })
-      .catch(function (error) {
-        // Handle Errors here.
-        var errorMessage = error.message;
-        window.alert("Error: " + errorMessage);
-      });
-
+  $(document).on("click", "#profile-submit", function () {
+    event.preventDefault();
     curUser.firstName = $("#firstname").val();
     curUser.lastName = $("#lastname").val();
     curUser.username = $("#username").val();
@@ -189,94 +154,249 @@ $(document).ready(function () {
     curUser.steamName = $("#steam-name").val();
     curUser.psnName = $("#psn-name").val();
     curUser.xboxName = $("#gamer-tag").val();
+    curUser.nintendoId = $("#nintendo-id").val();
+    curUser.aboutMe = $("#about-input").val();
 
-    var database = firebase.database();
-    database.ref('users/' + uid).set({
-      username: curUser.username,
-      firstname: curUser.firstName,
-      lastName: curUser.lastName,
-      email: curUser.email,
-      steamName: curUser.steamName,
-      psnName: curUser.psnName,
-      xboxName: curUser.xboxName,
-    });
-
-  });
+    // set local storage
+    localStorage.setItem("firstname", curUser.firstName);
+    localStorage.setItem("lastname", curUser.lastName);
+    localStorage.setItem("username", curUser.username);
 
 
+    // store data into firebase  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    database.ref("users/" + curUser.username).update({
+      
+        username: curUser.username,
+        firstname: curUser.firstName,
+        lastName: curUser.lastName,
+        email: curUser.email,
+        steamName: curUser.steamName,
+        psnName: curUser.psnName,
+        xboxName: curUser.xboxName,
+      
 
-  $("#login-btn").click(function (user) {
-    var user = firebase.auth().currentUser;
-    var email = $("#login-email").val().trim();
-    var password = $("#login-password").val().trim();
-    $("#login-email").val("");
-    $("#login-password").val("");
-    if (user != null) {
-      console.log("Hi!");
-      window.alert("You are already signed into an account. Please sign out before attempting to log into a different account.");
-    } else {
-      firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
-        .then(function () {
-          // Existing and future Auth states are now persisted in the current
-          // session only. Closing the window would clear any existing state even
-          // if a user forgets to sign out.
-          // ...
-          // New sign-in will be persisted with session persistence.
-          return firebase.auth().signInWithEmailAndPassword(email, password);
-        })
-        .catch(function (error) {
-          // Handle Errors here.
-          var errorMessage = error.message;
-          window.alert("Error: " + errorMessage);
-        });
+    })
+  })
+  // Profile creation end <<<=================================================================
 
 
-    }
-
-  });
-  firebase.auth().onAuthStateChanged(user => {
-    if (user) {
-      window.location = 'profile.html'; //After successful login, user will be redirected to home.html
-    }
-  });
-
-  $("#signout-btn").click(function () {
-    firebase.auth().signOut();
-    localStorage.clear();
-    window.location = "startpage.html";
-
-  });
-
-  //very last brackets (Document Ready) below here!  
-  // $(document).on("click", "#submit-btn", function () {
-  //   event.preventDefault();
-  //   var uid = firebase.auth().currentUser;
-  //   curUser.firstName = $("#firstname").val();
-  //   curUser.lastName = $("#lastname").val();
-  //   curUser.username = $("#username").val();
-  //   curUser.email = $("#email").val();
-  //   curUser.steamName = $("#steam-name").val();
-  //   curUser.psnName = $("#psn-name").val();
-  //   curUser.xboxName = $("#gamer-tag").val();
-  //   curUser.nintendoId = $("#nintendo-id").val();
-  //   curUser.aboutMe = $("#about-input").val();
-
-  //   // print and format data to DIV containing user info on profile page
-  //   // store data into firebase
-  //   database.ref("users/" + localStorage.getItem("UID")).update({
-  //     username: curUser.username,
-  //     firstname: curUser.firstName,
-  //     lastName: curUser.lastName,
-  //     email: curUser.email,
-  //     steamName: curUser.steamName,
-  //     psnName: curUser.psnName,
-  //     xboxName: curUser.xboxName,
 
 
-  //   })
+  // steam API start ===============================================================================================>>>>
 
-  //   window.location = "profile.html";
-  // })
 
+  // find steam id from create profile page
+  $(document).on("click", "#get-steam", function () {
+    event.preventDefault();
+    var vanityName = $("#steam-name").val().trim();
+    getSteamName(vanityName)
+  })
+  function getSteamName(name) {
+    // Fetches steam user ID number
+    var queryURL = "https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=597FC535B0A81C139B5227A808EAA15B&vanityurl=" + name
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (data) {
+
+      var steamNumber = data.response.steamid;
+
+      returnSteamName(steamNumber);
+
+    })
+  }
+  function returnSteamName(id) {
+    var queryURL = "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=597FC535B0A81C139B5227A808EAA15B&steamids=" + id;
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (data) {
+      var steamName = data.response.players[0].personaname;
+      console.log(steamName)
+      $("#steam-name").val(steamName);
+      curUser.avatar = data.response.players[0].avatarmedium;
+     
+      //  $("#image-share").html(image);
+      var lastLogOff = data.response.players[0].lastlogoff;
+      var newDate = $.parseJSON(lastLogOff);
+      var formatDate = new Date(1000 * newDate);
+      curUser.steamLastOnline = formatDate
+      // store in firebase  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+      // check if online 
+      var online = data.response.players[0].personastate;
+      if (online === 0) {
+        curUser.steamOnline = "no"
+      } else if (online === 1) {
+        curUser.steamOnline = "yes"
+      }
+      // store in firebase  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    })
+  }
+
+// steam API end <<<================================================================================
+
+
+
+  
+
+// populate profile page start ==================================================================>>>>
+
+// need to add onload="populatePage();" to <body> of page <---------------------!!!!!!
+
+
+function populatePage(){
+
+// This will all be populated with firebase!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// username
+
+// $("#nameOfUser").html("<h1>" + firebase variable will go here + "</h1>") 
+
+
+// screen names
+
+
+// status
+
+// game library
+
+// Gamer ID card
+
+// contact
+
+// 
+}
+// populate profile page end  <<<<==================================================================
+
+
+
+// gamer id card start ==================================================================>>>>
+
+// need to add onload="draw();" to body of page <---------------------!!!!!!
+function draw() {
+  // text
+  var par = document.getElementById('canvas').getContext('2d');
+  par.font = '30px serif';
+  par.fillText("Gamer ID Card", 8, 40);
+  // psn name
+  if (curUser.psnName !== undefined) {
+    par.fillText("psn: " + curUser.psnName, 220, 100);
+  } else {
+    par.fillText("psn: none", 220, 100);
+  }
+  // xbox name
+  if (curUser.xboxName !== undefined) {
+    par.fillText("xbox: " + curUser.xboxName, 220, 150);
+  } else {
+    par.fillText("xbox: none", 220, 150);
+  }
+  // steam name
+  if (curUser.steamName !== undefined) {
+    par.fillText("steam: " + curUser.steamName, 220, 200);
+  } else {
+    par.fillText("steam: none", 220, 200);
+  }
+
+  // avatar image
+  var ctx = canvas.getContext('2d');
+  ctx.drawImage(document.getElementById('image-source'), 8, 60, 180, 180)
+}
+
+// gamer id card ends  <<<==================================================================
+
+// start page floating button
+$('.fixed-action-btn').floatingActionButton();
+
+// tabs
+$('.tabs').tabs();
+
+
+
+$('.fixed-action-btn').floatingActionButton();
+  
+
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true,
+  indicators: true,
+  next: 3
+});
+
+$('.sidenav').sidenav();
+$('.tabs').tabs({ swipeable: true });
+
+setInterval(function () {
+  $('.carousel').carousel('next');
+}, 5000);
+
+
+$("#signout-btn").click(function () {
+  firebase.auth().signOut();
+  localStorage.clear();
+  window.location = "startpage.html";
 
 });
+
+
+})
+
+
+
+
+
+
+
+
+// // Steam api =========================================================================================================================
+  
+// function getSteamId(steamId) {
+//   // Fetch steam user ID number
+//   var queryURL = "https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=597FC535B0A81C139B5227A808EAA15B&vanityurl=" + steamId
+//   $.ajax({
+//     url: queryURL,
+//     method: "GET"
+//   }).then(function (data) {
+
+//     var steamNumber = data.response.steamid;
+
+//     getSteamProfile(steamNumber);
+
+//   })
+// }
+// //  get steam ID from vanity URL search
+// $("#steam-submit").on("click", function () {
+//   event.preventDefault();
+//   var steamId = $("#steam-input").val().trim();
+
+//   getSteamId(steamId);
+// })
+
+
+// // Fetch steam user profile page
+
+// // nick's steam ID for testing purposes: 76561197972752173
+
+// function getSteamProfile(number) {
+//   var queryURL = "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=597FC535B0A81C139B5227A808EAA15B&steamids=" + number;
+//   $.ajax({
+//     url: queryURL,
+//     method: "GET"
+//   }).then(function (data) {
+//     // save avatar
+//     curUser.avatar = data.response.players[0].avatarmedium;
+//     // last online
+//     var lastLogOff = data.response.players[0].lastlogoff;
+//     var newDate = $.parseJSON(lastLogOff);
+//     var formatDate = new Date(1000 * newDate);
+//     curUser.steamLastOnline = formatDate
+//     // check if online 
+//     var online = data.response.players[0].personastate;
+//     if (online === 0) {
+//       curUser.steamOnline = "no"
+//     } else if (online === 1) {
+//       curUser.steamOnline = "yes"
+//     }
+    
+//   })
+// }
+// // ==================================================================================================================================
---
+++ assets/javascript/script.js
@@ -0,0 +1,261 @@
+
+
+$(document).ready(function () {
+  var config = {
+    apiKey: "AIzaSyBZvq3N02QOEG6fa-6qiDLwOHoSeIBza0Q",
+    authDomain: "game-website-9813e.firebaseapp.com",
+    databaseURL: "https://game-website-9813e.firebaseio.com",
+    projectId: "game-website-9813e",
+    storageBucket: "game-website-9813e.appspot.com",
+    messagingSenderId: "70919236665"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  var curUser = {
+    username: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    avatar: "",
+    steamName: "",
+    psnName: "",
+    xboxName: "",
+    nintendoId: "",
+    aboutMe: "",
+    status: "",
+    steamOnline: "",
+    steamLastOnline: ""
+  }
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true,
+    next: 3
+  });
+
+  setInterval(function () {
+    $('.carousel').carousel('next');
+  }, 5000);
+
+  $('.tabs').tabs({ swipeable: true });
+
+  $('.modal').modal();
+
+  $('#profile-modal').on("click", function () {
+
+    $('#modal1').modal('open');
+
+  });
+
+  $("#formValidate").validate({
+    rules: {
+      firstname: {
+        required: true
+      },
+      lastname: {
+        required: true
+      },
+      username: {
+        required: true,
+        minlength: 5
+      },
+      pwd: {
+        required: true,
+        minlength: 5
+      },
+      conf_pwd: {
+        required: true,
+        minlength: 5,
+        equalTo: "#id_pwd"
+      },
+    },
+    //For custom messages
+    messages: {
+      firstname: { required: "Enter a First Name" },
+      lastname: { required: "Enter a Last Name" },
+      username: {
+        required: "Enter a Last Name",
+        minlength: "Must be at least 5 characters"
+      },
+      pwd: {
+        required: "Enter a Password",
+        minlength: "Must be at least 5 characters"
+      },
+      conf_pwd: {
+        required: "Re-Enter Password",
+        minlength: "Must be at least 5 characters",
+        equalTo: "Password does not match"
+      }
+
+    },
+    errorElement: 'div',
+    errorPlacement: function (error, element) {
+      var placement = $(element).data('error');
+      if (placement) {
+        $(placement).append(error)
+      } else {
+        error.insertAfter(element);
+      }
+    }
+  });
+
+
+  firebase.auth().onAuthStateChanged(function (user) {
+    var user = firebase.auth().currentUser;
+    var email, uid;
+
+    curUser.firstName = $("#firstname").val();
+    curUser.lastName = $("#lastname").val();
+    curUser.username = $("#username").val();
+    curUser.email = $("#email").val();
+    curUser.steamName = $("#steam-name").val();
+    curUser.psnName = $("#psn-name").val();
+    curUser.xboxName = $("#gamer-tag").val();
+    curUser.nintendoId = $("#nintendo-id").val();
+    curUser.aboutMe = $("#about-input").val();
+
+
+
+    if (!user) {
+      event.preventDefault();
+    }
+    else if (user) {
+      // User is signed in.
+      console.log("There's a user signed in!");
+      $("#logged-in").css("display", "inline-block");
+      $("#not-logged-in").css("display", "none");
+
+      var email = user.email;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      var database = firebase.database();
+      database.ref('users/' + uid).set({
+        username: curUser.username,
+        firstname: curUser.firstName,
+        lastName: curUser.lastName,
+        email: curUser.email,
+        steamName: curUser.steamName,
+        psnName: curUser.psnName,
+        xboxName: curUser.xboxName,
+      });
+
+      console.log(email, isAnonymous, uid, providerData);
+      localStorage.setItem("username", username);
+      localStorage.setItem("UID", uid);
+
+
+      $("#welcome-text").text(" Happy to have you here, " + email + "!");
+      console.log(name, email, uid);
+
+    } else {
+      // No user is signed in.
+      console.log("Nobody's here!");
+      $("#logged-in").css("display", "none");
+      $("#not-logged-in").css("display", "inline-block");
+      $("#welcome-text").empty();
+    }
+  });
+
+  $("#submit-btn").click(function (user) {
+    var email = $("#email").val().trim();
+    var password = $("#id_pwd").val().trim();
+    if (user != null) {
+      firebase.auth().signOut();
+    }
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+      .then(function () {
+        // Existing and future Auth states are now persisted in the current
+        // session only. Closing the window would clear any existing state even
+        // if a user forgets to sign out.
+        // ...
+        // New sign-in will be persisted with session persistence.
+        return firebase.auth().createUserWithEmailAndPassword(email, password);
+      })
+      .catch(function (error) {
+        // Handle Errors here.
+        var errorMessage = error.message;
+        window.alert("Error: " + errorMessage);
+      });
+    
+  });
+
+
+
+  $("#login-btn").click(function (user) {
+    var user = firebase.auth().currentUser;
+    var email = $("#login-email").val().trim();
+    var password = $("#login-password").val().trim();
+    $("#login-email").val("");
+    $("#login-password").val("");
+    if (user != null) {
+      console.log("Hi!");
+      window.alert("You are already signed into an account. Please sign out before attempting to log into a different account.");
+    } else {
+      firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+        .then(function () {
+          // Existing and future Auth states are now persisted in the current
+          // session only. Closing the window would clear any existing state even
+          // if a user forgets to sign out.
+          // ...
+          // New sign-in will be persisted with session persistence.
+          return firebase.auth().signInWithEmailAndPassword(email, password);
+        })
+        .catch(function (error) {
+          // Handle Errors here.
+          var errorMessage = error.message;
+          window.alert("Error: " + errorMessage);
+        });
+        
+      
+    }
+  
+  });
+  firebase.auth().onAuthStateChanged(user => {
+    if(user) {
+      window.location = 'profile.html'; //After successful login, user will be redirected to home.html
+    }
+  });
+
+  $("#signout-btn").click(function () {
+    firebase.auth().signOut();
+    localStorage.clear();
+    window.location = "startpage.html";
+
+  });
+
+  //very last brackets (Document Ready) below here!  
+  // $(document).on("click", "#submit-btn", function () {
+  //   event.preventDefault();
+  //   var uid = firebase.auth().currentUser;
+  //   curUser.firstName = $("#firstname").val();
+  //   curUser.lastName = $("#lastname").val();
+  //   curUser.username = $("#username").val();
+  //   curUser.email = $("#email").val();
+  //   curUser.steamName = $("#steam-name").val();
+  //   curUser.psnName = $("#psn-name").val();
+  //   curUser.xboxName = $("#gamer-tag").val();
+  //   curUser.nintendoId = $("#nintendo-id").val();
+  //   curUser.aboutMe = $("#about-input").val();
+
+  //   // print and format data to DIV containing user info on profile page
+  //   // store data into firebase
+  //   database.ref("users/" + localStorage.getItem("UID")).update({
+  //     username: curUser.username,
+  //     firstname: curUser.firstName,
+  //     lastName: curUser.lastName,
+  //     email: curUser.email,
+  //     steamName: curUser.steamName,
+  //     psnName: curUser.psnName,
+  //     xboxName: curUser.xboxName,
+
+
+  //   })
+
+  //   window.location = "profile.html";
+  // })
+
+
+});
