929035077451bc4195bdaec16cf438810dda8637
Castracani <willmyers993@gmail.com>
2018-07-06 19:15:16 -0400

    please

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -34,13 +34,17 @@ var curUser = {
 $(document).ready(function () {
 
   var user = firebase.auth().currentUser;
-
-if (user) {
+  var name, email, photoUrl, uid, emailVerified;
   
-  // User is signed in.
-} else {
-  // No user is signed in.
-}
+  if (user != null) {
+    name = user.displayName;
+    email = user.email;
+    photoUrl = user.photoURL;
+    emailVerified = user.emailVerified;
+    uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
+                     // this value to authenticate with your backend server, if
+                     // you have one. Use User.getToken() instead.
+  }
 
 
   
