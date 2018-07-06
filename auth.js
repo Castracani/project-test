48b1a2d9dbb6e1faa2b17e7d3ee485c26f6701e8
Castracani <willmyers993@gmail.com>
2018-07-06 12:31:58 -0400

    sharing(!!!)

--- auth.js
+++ auth.js
@@ -1,19 +1,96 @@
-var uiConfig = {
-  signInSuccessUrl: 'https://castracani.github.io/Project-1/profile.html',
-  signInOptions: [
-    // Leave the lines as is for the providers you want to offer your users.
-    {
-      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      requireDisplayName: true
-    },
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-  ],
-};
 
 
-// Initialize the FirebaseUI Widget using Firebase.
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
-firebase.auth.setPersistence(firebase.auth.Auth.Persistence.SESSION);
-// The start method will wait until the DOM is loaded.
-ui.start('#firebaseui-auth-container', uiConfig);
+$(document).ready(function () {
+
+  firebase.auth().onAuthStateChanged(function (user) {
+    var user = firebase.auth().currentUser;
+    var email, uid;
+
+
+    if (user) {
+      // User is signed in.
+      console.log("There's a user signed in!");
+      $("#logged-in").css("display", "inline-block");
+      $("#not-logged-in").css("display", "none");
+      var email = user.email;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      var database = firebase.database();
+      database.ref('users/' + uid).set({
+        username: name,
+        email: email
+      });
+
+      console.log(email, isAnonymous, uid, providerData);
+
+
+      $("#welcome-text").text(" Happy to have you here, " + email + "!");
+      console.log(name, email, uid);
+
+    } else {
+      // No user is signed in.
+      console.log("Nobody's here!");
+      $("#logged-in").css("display", "none");
+      $("#not-logged-in").css("display", "inline-block");
+      $("#welcome-text").empty();
+    }
+  });
+
+  $("#submit-btn").click(function (user) {
+    var email = $("#email").val().trim();
+    var password = $("#password").val().trim();
+    $("#email").val("");
+    $("#password").val("");
+    if (user != null) {
+      firebase.auth().signOut();
+    }
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+      .then(function () {
+        // Existing and future Auth states are now persisted in the current
+        // session only. Closing the window would clear any existing state even
+        // if a user forgets to sign out.
+        // ...
+        // New sign-in will be persisted with session persistence.
+        return firebase.auth().createUserWithEmailAndPassword(email, password);
+      })
+      .catch(function (error) {
+        // Handle Errors here.
+        var errorMessage = error.message;
+        window.alert("Error: " + errorMessage);
+      });
+  });
+
+  $("#login-btn").click(function (user) {
+    var user = firebase.auth().currentUser;
+    var email = $("#email").val().trim();
+    var password = $("#password").val().trim();
+    $("#email").val("");
+    $("#password").val("");
+    if (user != null) {
+      console.log("Hi!");
+      window.alert("You are already signed into an account. Please sign out before attempting to log into a different account.");
+    } else {
+      firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+        .then(function () {
+          // Existing and future Auth states are now persisted in the current
+          // session only. Closing the window would clear any existing state even
+          // if a user forgets to sign out.
+          // ...
+          // New sign-in will be persisted with session persistence.
+          return firebase.auth().signInWithEmailAndPassword(email, password);
+        })
+        .catch(function (error) {
+          // Handle Errors here.
+          var errorMessage = error.message;
+          window.alert("Error: " + errorMessage);
+        });
+    }
+  });
+
+  $("#signout-btn").click(function () {
+    firebase.auth().signOut();
+  });
+
+  //very last brackets (Document Ready) below here!  
+});
